bd43f6e02fef5c1cbbaa053a439e19b286b7bc0f
Augustin <augustinusmp@gmail.com>
2026-02-02 18:48:01 -0500

    add generators

--- graph_operations/generators.py
+++ graph_operations/generators.py
@@ -4,3 +4,18 @@ import networkx as nx
 def create_graph():
     G = nx.complete_graph(100)
     return G
+
+
+def create_erdos_renyi_graph(n=100, p=0.05):
+    G = nx.erdos_renyi_graph(n, p)
+    return G
+
+
+def create_small_world_graph(n=100, k=4, p=0.1):
+    G = nx.watts_strogatz_graph(n, k, p)
+    return G
+
+
+def create_scale_free_graph(n=100):
+    G = nx.barabasi_albert_graph(n, 3)
+    return G
